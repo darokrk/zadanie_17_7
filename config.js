5233de66e6c34711814bb0f79c72de573579b569
Dariusz Rosiek <daro.krk@interia.pl>
2018-04-04 12:20:26 +0200

    Create file config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '996705482606-8hlm7gcsu4a3ip3oaagafa38pp3eh16n.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_ID': 'YEcaGPftp__rCh_eJchE1ftA',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
